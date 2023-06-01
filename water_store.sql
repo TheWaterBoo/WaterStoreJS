2e6aee20880bb8b2281000cb8802ca7ffef58162
Pedro Lopez Ramirez <thedrasticpower@gmail.com>
2023-05-31 23:07:46 -0600

    Added modified script
    the last script doesn't includes the table 'usuarios' and in this script has been added

--- water_store.sql
+++ water_store.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 03-04-2022 a las 23:05:57
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-11-2022 a las 03:36:25
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,15 +39,36 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`id`, `articulo`, `precio`, `cantidad`) VALUES
-(1, 'Camiseta Kirby', 250, 19),
-(2, 'Camiseta Nuclear Throne - Rouge', 200, 1),
-(3, 'Camiseta GUN GODZ - God of guns', 150, 2),
-(4, 'Camiseta Nuclear Throne - Y.V', 120, 5),
-(5, 'Camiseta Nuclear Throne - Melting', 150, 2),
-(6, 'Camiseta Super Meat Boy - Meat Boy', 200, 1),
-(7, 'Camiseta programador', 100, 12),
-(8, 'Camiseta pyhton', 100, 5),
-(9, 'Camiseta cicle', 100, 12);
+(1, 'Chaqueta cafe', 400, 15),
+(2, 'Sudadera blanco con negro', 400, 23),
+(3, 'Camiseta de vestir Azul', 500, 10),
+(4, 'Camiseta de vestir blanca', 600, 19),
+(5, 'Traje color azul rey', 700, 20),
+(6, 'Sudadera rojo carmesi', 600, 15),
+(7, 'Zapatos de vestir', 900, 11),
+(8, 'Short casual', 300, 17),
+(9, 'Tenis para correr', 800, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(10) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `password`, `email`) VALUES
+(13, 'admin', '$2y$10$aCvkuTlIbPwnmzh5e77cJ.e8jrpK.3CISCR1w9FZXJQUMZwjNO4Pa', 'admin@correo.com'),
+(14, 'usuario', '$2y$10$bDMq98b1iuvo8HgEBS7bduDvCeasqTLd71.1z03bMLMuS5atwayWS', 'user@usermail.com');
 
 --
 -- Índices para tablas volcadas
@@ -60,6 +81,13 @@ ALTER TABLE `producto`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -68,6 +96,12 @@ ALTER TABLE `producto`
 --
 ALTER TABLE `producto`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
